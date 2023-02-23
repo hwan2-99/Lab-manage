c2785bb7d1de2147360b1ea248b9c6dddd3026d1
hwan2-99 <thh1233123@gmail.com>
2023-02-23 11:18:47 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/dnlab/DnLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.dnlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnLabApplication.class, args);
+    }
+
+}
